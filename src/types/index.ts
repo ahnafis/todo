388a31e0c3f7581590a12cbb6016b59ee27066c6
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2024-11-23 01:17:45 +0000

    types: Remove nullable types (prefer Partial<T>)

--- src/types/index.ts
+++
@@ -1,4 +0,0 @@
-import type { Entity, NonEntity, TimeStamp, UniqueId } from "./entity";
-import type { DeepNullable, Nullable } from "./nullable";
-
-export type { DeepNullable, Entity, Nullable, NonEntity, TimeStamp, UniqueId };
--- src/types/nullable.ts
+++
@@ -1,6 +0,0 @@
-export type Nullable<T> = T | null | undefined;
-
-// Make the values T[K] of type T Nullable.
-export type DeepNullable<T> = {
-  [K in keyof T]: Nullable<T[K]>;
-};
