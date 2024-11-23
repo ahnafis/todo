9feddbb68c6f98f5d6915fa612d53a79e32b26a9
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2024-11-23 02:53:04 +0000

    fix(feat/tasks): Pass filters in the get use case

--- src/features/tasks/domain/use_cases/GetTasks.ts
+++ src/features/tasks/domain/use_cases/GetTasks.ts
@@ -6,7 +6,7 @@ import type { ITaskRepository } from "../repositories";
 export default class GetTasks implements Command<Partial<Task>, Task[]> {
   constructor(private repository: ITaskRepository) {}
 
-  execute(filter?: Partial<Task>): Task[] {
-    return this.repository.getTasks(filter);
+  execute(filters?: Partial<Task>): Task[] {
+    return this.repository.getTasks(filters);
   }
 }
