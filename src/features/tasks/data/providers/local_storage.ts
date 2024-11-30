68898d81e0f98cbb0dd76de6bb25f6cb57db4b45
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2024-11-30 23:18:42 +0600

    create wrapper class for local storage
    separate local storage methods from TaskLocalDataProvider

--- src/features/tasks/data/providers/local_storage.ts
+++ src/features/tasks/data/providers/local_storage.ts
@@ -1,61 +1,28 @@
 import type { UniqueId } from "@/types";
 import type { Task } from "../../domain/entities";
 import ITaskDataProvider from "./ITaskDataProvider";
+import { LocalStorageClient } from "@/utils";
 
 export default class TaskLocalDataProvider implements ITaskDataProvider {
-  private db: Task[];
+  private db;
 
-  constructor(private collection: string) {
-    const raw_data: string = localStorage.getItem(this.collection) || "[]";
-    this.db = JSON.parse(raw_data) || [];
+  constructor(collection: string) {
+    this.db = new LocalStorageClient<Task>(collection);
   }
 
   public insert = async (data: Task): Promise<void> => {
-    if (await this.exists(data.id)) {
-      // TODO: Throw and handle an error instead.
-      return console.log("Cannot add same item agian.");
-    }
-
-    this.db.push(data);
-    await this.save();
+    await this.db.insert(data);
   };
 
   public read = async (filters?: Partial<Task>): Promise<Task[]> => {
-    if (!filters) return this.db;
-
-    return this.db.filter((task) => {
-      let key: keyof Partial<Task>;
-
-      // Return if filters matches with any data in the database.
-      for (key in filters) return task[key] === filters[key];
-    });
+    return await this.db.read(filters);
   };
 
   public update = async (new_data: Task): Promise<void> => {
-    const index = this.db.indexOf(new_data);
-    const exists = index > -1;
-
-    // TODO: Throw and handle an error instead.
-    if (!exists) return console.log("Not matches found. Nothing to update.");
-
-    this.db[index] = new_data;
-    await this.save();
+    await this.db.update(new_data);
   };
 
   public delete = async (id: UniqueId): Promise<void> => {
-    const matches = await this.read({ id });
-    const index = this.db.indexOf(matches[0]);
-
-    this.db.splice(index, 1);
-    await this.save();
-  };
-
-  private exists = async (id: UniqueId): Promise<boolean> => {
-    const matches = await this.read({ id });
-    return matches.length > 0;
-  };
-
-  private save = async (): Promise<void> => {
-    localStorage.setItem(this.collection, JSON.stringify(this.db));
+    await this.db.delete(id);
   };
 }
---
+++ src/utils/local_storage_client.ts
@@ -0,0 +1,59 @@
+import type { Entity, UniqueId } from "@/types";
+
+export default class LocalStorageClient<T extends Entity> {
+  private db: T[];
+
+  constructor(private collection: string) {
+    const raw_data: string = localStorage.getItem(this.collection) || "[]";
+    this.db = JSON.parse(raw_data) || [];
+  }
+
+  public insert = async (data: T): Promise<void> => {
+    if (await this.exists(data.id)) {
+      // TODO: Throw and handle an error instead.
+      return console.log("Cannot add same item agian.");
+    }
+
+    this.db.push(data);
+    await this.save();
+  };
+
+  public read = async (filters?: Partial<T>): Promise<T[]> => {
+    if (!filters) return this.db;
+
+    return this.db.filter((task) => {
+      let key: keyof Partial<T>;
+
+      // Return if filters matches with any data in the database.
+      for (key in filters) return task[key] === filters[key];
+    });
+  };
+
+  public update = async (new_data: T): Promise<void> => {
+    const index = this.db.indexOf(new_data);
+    const exists = index > -1;
+
+    // TODO: Throw and handle an error instead.
+    if (!exists) return console.log("Not matches found. Nothing to update.");
+
+    this.db[index] = new_data;
+    await this.save();
+  };
+
+  public delete = async (id: UniqueId): Promise<void> => {
+    const matches = await this.read({ id } as Partial<T>);
+    const index = this.db.indexOf(matches[0]);
+
+    this.db.splice(index, 1);
+    await this.save();
+  };
+
+  private exists = async (id: UniqueId): Promise<boolean> => {
+    const matches = await this.read({ id } as Partial<T>);
+    return matches.length > 0;
+  };
+
+  private save = async (): Promise<void> => {
+    localStorage.setItem(this.collection, JSON.stringify(this.db));
+  };
+}
